7cd926c95f7d0b8ae766d6305572889aa3665e90
sunbo <sunbo@aiyaapp.com>
2018-12-09 23:14:39 +0800

    add 4.8.py

---
+++ 4.8.py
@@ -0,0 +1,88 @@
+# -*- coding:utf-8 -*-
+import os
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow.python.tools.inspect_checkpoint import print_tensors_in_checkpoint_file
+
+def moving_average(a, w=10):
+    if len(a) < w:
+        return a[:]
+    return [val if idx < w else sum(a[(idx-w) : idx]) / w for idx, val in enumerate(a)]
+
+train_X = np.linspace(-1, 1, 100)
+train_Y = 2 * train_X + np.random.randn(*train_X.shape) * 0.3
+
+plt.figure(1)
+plt.subplot(211)
+plt.plot(train_X, train_Y, 'ro', label='Original data')
+
+tf.reset_default_graph()
+
+inputdict = {
+    'x': tf.placeholder('float'),
+    'y': tf.placeholder('float')
+}
+
+paradict = {
+    'w': tf.Variable(tf.random_normal([1]), name='weight'),
+    'b': tf.Variable(tf.zeros([1]), name='bias')
+}
+
+z = tf.multiply(inputdict['x'], paradict['w']) + paradict['b']
+tf.summary.histogram('z', z)
+
+cost = tf.reduce_mean(tf.square(inputdict['y'] - z))
+tf.summary.scalar('loss_function', cost)
+
+learning_rate = 0.01
+optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
+
+init = tf.global_variables_initializer()
+
+training_epochs = 20
+display_step = 2
+
+saver = tf.train.Saver()
+with tf.Session() as sess:
+    sess.run(init)
+    merged_summary_op = tf.summary.merge_all()
+    summary_writer = tf.summary.FileWriter(os.path.join('log', 'summaries'), sess.graph)
+
+    plotdata = {'batchsize':[], 'loss':[], 'avgloss':[]}
+    for epoch in range(training_epochs):
+        for (x, y) in zip(train_X, train_Y):
+            sess.run(optimizer, feed_dict={inputdict['x']:x, inputdict['y']:y})
+            summary_str = sess.run(merged_summary_op, feed_dict={inputdict['x']:x, inputdict['y']:y})
+            summary_writer.add_summary(summary_str, epoch)
+
+        if epoch % display_step == 0:
+            # loss = sess.run(cost, feed_dict={inputdict['x']:train_X, inputdict['y']:train_Y})
+            loss = cost.eval({inputdict['x']:train_X, inputdict['y']:train_Y})
+            print('epoch', epoch+1, 'cost =', loss, 'W =', sess.run(paradict['w']), 'b =', sess.run(paradict['b']))
+            if not (loss == 'NA'):
+                plotdata['batchsize'].append(epoch)
+                plotdata['loss'].append(loss)
+
+    print('Finished!')
+    print('cost =', loss, 'w =', sess.run(paradict['w']), 'b =', sess.run(paradict['b']))
+    plt.plot(train_X, sess.run(paradict['w']) * train_X + sess.run(paradict['b']), 'b+', label='Fitted_data')
+    plt.legend()
+
+    plotdata['avgloss'] = moving_average(plotdata['loss'])
+    plt.subplot(212)
+    plt.plot(plotdata['batchsize'], plotdata['avgloss'], 'b--')
+    plt.xlabel('Minibatch number')
+    plt.ylabel('loss')
+
+    print('x = 0.2, z =', sess.run(z, feed_dict={inputdict['x']:0.2}))
+    saver.save(sess, os.path.join('checkpoint', str(epoch)))
+
+plt.show()
+
+saver = tf.train.Saver()
+with tf.Session() as sess2:
+    sess2.run(tf.global_variables_initializer())
+    saver.restore(sess2, os.path.join('checkpoint', str(epoch)))
+    print_tensors_in_checkpoint_file(os.path.join('checkpoint', str(epoch)), None, True)
+    print('x = 0.2, z =', sess2.run(z, feed_dict={inputdict['x']:0.2}))
