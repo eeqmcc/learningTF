30ab4078d05dec0a0636ddc0172f5212a1fb8e57
sunbo <sunbo@aiyaapp.com>
2018-12-23 23:14:57 +0800

    decay learning rate

--- 7.5.py
+++ 7.5.py
@@ -16,6 +16,8 @@ n_classes = 10
 x = tf.placeholder(tf.float32, [None, n_input])
 y = tf.placeholder(tf.float32, [None, n_classes])
 keep_prob = tf.placeholder(tf.float32)
+global_step = tf.Variable(0, trainable=False)
+add_global = global_step.assign_add(1)
 
 def multilayer_perception(x, weights, biases, keep_prob):
     layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
@@ -45,7 +47,9 @@ pred = multilayer_perception(x, weights, biases, keep_prob)
 
 reg = 0.01
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=y)) + reg * (tf.nn.l2_loss(weights['h1']) + tf.nn.l2_loss(weights['h2']))
-optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+decay_rate = tf.train.exponential_decay(learning_rate, global_step, 8000, 0.9)
+# optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+optimizer = tf.train.AdamOptimizer(decay_rate).minimize(cost, global_step=global_step)
 
 
 def oneHot(label, c):
@@ -67,7 +71,7 @@ print('y_train.shape', y_train.shape)
 print('x_test.shape', x_test.shape)
 print('y_test.shape', y_test.shape)
 
-training_epochs = 100
+training_epochs = 400
 batch_size = 100
 display_step = 1
 saver = tf.train.Saver()
@@ -82,7 +86,7 @@ with tf.Session() as sess:
             x_batch = np.reshape(x_train[i * batch_size : (i + 1) * batch_size, :], (batch_size, -1))
             y_batch = y_train[i * batch_size : (i + 1) * batch_size]
             _, c = sess.run([optimizer, cost], feed_dict={x: x_batch, y: y_batch, keep_prob: 0.7})
-            p = sess.run(pred, feed_dict={x: x_batch, keep_prob: 1.0})
+            p = sess.run([pred, add_global], feed_dict={x: x_batch, keep_prob: 1.0})
             avg_cost += c / total_batch
         if (epoch + 1) % display_step == 0:
             correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
